825979051c5793e6220a46e66d3b64eff702dfb9
estagent <estagent@chip.me>
2020-12-28 08:24:28 +0300

    package updated

--- src/index.js
+++ src/index.js
@@ -7,20 +7,11 @@ const options = {
 /**
  * @returns {boolean}
  */
-const isMuted = () => Preference.get('muted') === true;
-const mute = (flag = true) => Preference.set('muted', flag);
-const toggleMute = () => {
+export const isMuted = () => Preference.get('muted') === true;
+export const mute = (flag = true) => Preference.set('muted', flag);
+export const toggleMute = () => {
   mute(!isMuted());
-  const eventAwesome = new CustomEvent(options.toggled, {
+  window.dispatchEvent(new CustomEvent(options.toggled, {
     isMuted: isMuted,
-    detail: {text: () => textarea.value},
-  });
+  }));
 };
-
-export default function() {
-  return {
-    isMuted: isMuted,
-    toggleMute: toggleMute,
-    mute: mute,
-  };
-}
